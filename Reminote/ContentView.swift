42a152b7ca2667702d38ea8f623fde3078b672a1
SamCorp <Manhcuong5311@gmail.com>
2026-03-24 21:04:25 +0700

    Initial Commit

---
+++ Reminote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reminote
+//
+//  Created by Sam Manh Cuong on 24/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reminote/ReminoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminoteApp.swift
+//  Reminote
+//
+//  Created by Sam Manh Cuong on 24/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ReminoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
